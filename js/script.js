672493ad4ef0aabd3770c15d27d1dc0d4725ff8b
mdeias <mdeias@icloud.com>
2021-10-26 16:49:55 +0200

    array e ciclo

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// definisco lista di chi può accedere alla mail con un array
+// chiedo all'utente di inserire la sua mail tramite promp
+// verifico che l'utente possa o meno accedere assegnando un valore numerico alla lista tramite un ciclo
+// stampo messaggio in base al risultato tramite innerhtml
+
+
+// 1 creo array con lista mail
+
+const mailist = [
+    "nomefittizio1@mail.com",
+    "nomefittizio2@mail.com",
+    "nomefittizio3@mail.com",
+    "nomefittizio4@mail.com",
+    "nomefittizio5@mail.com",
+    "nomefittizio6@mail.com",
+    "nomefittizio7@mail.com",
+    "nomefittizio8@mail.com",
+    "nomefittizio9@mail.com",
+    "nomefittizio10@mail.com",
+    "nomefittizio11@mail.com",
+    "nomefittizio12@mail.com",
+    "nomefittizio13@mail.com",
+    "nomefittizio14@mail.com",
+    "nomefittizio15@mail.com",
+    "nomefittizio16@mail.com",
+    "nomefittizio17@mail.com",
+    "nomefittizio18@mail.com",
+    "nomefittizio19@mail.com",
+    "nomefittizio20@mail.com"
+]
+
+// console.log(mailist);
+
+const mailutente = prompt ("Inserisci email")
+
+// assegno valore numerico alla lista mail con il ciclo for
+
+for (let i = 0; i < mailist.length; i++) {
+    //console.log(mailist[i]);
+    
+}
